70e7344c08e94d079883d9a8a8479d981ed1c575
Alpha Omar Barry <alphbarr@cbr8s5.42barcelona.com>
2024-02-22 11:22:51 +0100

    hexadecimals

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -11,33 +11,4 @@
 /* ************************************************************************** */
 #include <stdarg.h>
 #include <stdio.h>
-
-void my_printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 's': {
-                    char *string = va_arg(args, char*);
-                    printf("%s", string);
-                    break;
-                }
-                case 'd': {
-                    int num = va_arg(args, int);
-                    printf("%d", num);
-                    break;
-                }
-                default:
-                    printf("%c", *format);
-            }
-        } else {
-            putchar(*format);
-        }
-        format++;
-    }
-
-    va_end(args);
-}
+#include "ft_printf.h"
