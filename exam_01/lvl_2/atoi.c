84af8dd75e6f989bb09d60d5a490aec97a5010df
alpbarry <alphaomar9@gmail.com>
2024-03-29 23:22:54 +0100

    atoi

---
+++ exam_01/lvl_2/atoi.c
@@ -0,0 +1,28 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	m;
+	int	n;
+
+	i = 0;
+	m = 1;
+	n = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			m *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = (n * 10) + (str[i] - '0');
+		i++;
+	}
+	n *= m;
+	return (n);
+}
