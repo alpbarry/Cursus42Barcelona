a34de228e3909b4e6172e0cb5e5869636e2c14c9
Alpha Omar Barry <alphbarr@car12s4.42barcelona.com>
2024-04-16 16:27:15 +0200

    mainn

--- prueba_minilibx/objects/main.d
+++
@@ -1,2 +0,0 @@
-objects/main.o: sources/main.c sources/../includes/libft.h \
-  sources/../mlx/mlx.h
--- prueba_minilibx/sources/main.c
+++ prueba_minilibx/sources/main.c
@@ -1,13 +1,23 @@
-#include "../includes/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alphbarr <alphbarr@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 16:17:12 by alphbarr          #+#    #+#             */
+/*   Updated: 2024/04/16 16:19:10 by alphbarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../mlx/mlx.h"
-#include <stdlib.h>
+#include "../includes/libft.h"
 
-#include <stdio.h>
+int	main(void)
+{
+void	*mlx;
+	void	*mlx_win;
 
-int main( void ) {
-	char *prueba = malloc(sizeof(char) * 5);
-	prueba = "Hola";
-	char *hola = ft_strdup(prueba);
-	printf("Hola?? %s", hola);
-	return (1);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);	
 }
